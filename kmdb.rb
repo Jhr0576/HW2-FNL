3b9b0cc42cd10a6390853edbb96f48f9a82ec301
Jhr0576 <jacob.rice@kellogg.northwestern.edu>
2022-05-05 01:09:14 +0000

    Saved7

--- kmdb.rb
+++ kmdb.rb
@@ -131,27 +131,27 @@ new_actor11 = Actor.new
 new_actor11["name"] = "Anne Hathaway"
 new_actor11.save
 
-warner bros. = Studio.find_by({ "name" => "Warner Bros."})
+warner_bros. = Studio.find_by({ "name" => "Warner Bros."})
 
 new_movie = Movie.new
 new_movie["title"] = "Batman Begins"
 new_movie["year_released"] = "2005"
 new_movie["rated"] = "PG-13"
-new_movie["studio_id"] = warner bros.["id"]
+new_movie["studio_id"] = warner_bros.["id"]
 new_movie.save
 
 new_movie2 = Movie.new
 new_movie2["title"] = "The Dark Knight"
 new_movie2["year_released"] = "2008"
 new_movie2["rated"] = "PG-13"
-new_movie2["studio_id"] = warner bros.["id"]
+new_movie2["studio_id"] = warner_bros.["id"]
 new_movie2.save
 
 new_movie3 = Movie.new
 new_movie3["title"] = "The Dark Knight Rises"
 new_movie3["year_released"] = "2012"
 new_movie3["rating"] = "PG-13"
-new_movie3["studio_id"] = warner bros.["id"]
+new_movie3["studio_id"] = warner_bros.["id"]
 new_movie3.save
 
 batman begins = Movie.find_by({ "title" => "Batman Begins"})
